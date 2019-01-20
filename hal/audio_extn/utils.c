0e31a42f78e675998dd1f8773be197f806f91b30
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-01-20 17:48:20 +0100

    audio_extn: Fix unused parameter warning in utils.c
    The unused parameter warning appears if we are not building with
    at least one of the following cflags enabled:
    
    * DEV_ARBI_ENABLED
    * SOUND_TRIGGER_ENABLED
    * AUDIO_LISTEN_ENABLED
    
      hardware/qcom/audio/hal/audio_extn/utils.c:2522:55: error: unused parameter 'snd_device'
    [-Werror,-Wunused-parameter]
      void audio_extn_utils_release_snd_device(snd_device_t snd_device)
                                                          ^
    
    Change-Id: I694c683c9bfde60343f0f6ea8d806bc5e24437e6
    (cherry picked from commit 12981fb9f3c8a67bcc362c7badfb9189bc181cda)

--- hal/audio_extn/utils.c
+++ hal/audio_extn/utils.c
@@ -2912,6 +2912,7 @@ int audio_extn_utils_get_channels_from_string(const char *id_string)
     return -EINVAL;
 }
 
+#if defined(DEV_ARBI_ENABLED) || defined(SOUND_TRIGGER_ENABLED) || defined(AUDIO_LISTEN_ENABLED)
 void audio_extn_utils_release_snd_device(snd_device_t snd_device)
 {
     audio_extn_dev_arbi_release(snd_device);
@@ -3075,3 +3076,4 @@ size_t audio_extn_utils_get_input_buffer_size(uint32_t sample_rate,
 
     return size;
 }
+#endif
